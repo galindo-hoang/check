872b8223099c9c1df9d25b327da263c64e7b77c6
hoah <hmhuy191101@gmail.com>
2022-06-03 17:37:56 +0700

    hello

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -108,3 +108,107 @@ CREATE TABLE employee_monthly_vertec
 
 ALTER TABLE employee_monthly_vertec
     ADD CONSTRAINT FK_EMPLOYEEMONTHLYVERTEC_ON_VISA FOREIGN KEY (visa) REFERENCES employ_meta_info (visa);
+
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('ABC', 'DUMMY NAME 1', 'Name 1', 'Last Name 1', 'Dummy Division 1', 'Engineers', 'Group 1', 160.00, 160.00, 0.00, -3.58, 72.00, '12/16/2020', null, 132.00, 82.50, 20.00, 980, 'FALSE', 100, 0.0, 9, 'Engineers', '12/16/2020', 'Dummy Division 1', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('XYZ', 'DUMMY NAME 2', 'Name 2', 'Last Name 2', 'Dummy Division 2', 'Engineers', 'Group 2', 160.00, 160.00, 0.00, -8.50, 128.00, '1/18/2021', null, 136.00, 85.00, 16.00, 1000, 'FALSE', 100, 0.0, 16, 'Engineers', '1/18/2021', 'Dummy Division 2', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('DEF', 'DUMMY NAME 3', 'Name 3', 'Last Name 3', 'Dummy Division 3', 'Engineers', 'Group 3', 160.00, 160.00, 0.00, 0.00, 144.00, '11/2/2020', null, 152.00, 95.00, 0.00, 1000, 'FALSE', 100, 0.0, 18, 'Engineers', '11/2/2020', 'Dummy Division 3', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('XY1', 'DUMMY NAME 4', 'Name 4', 'Last Name 4', 'Dummy Division 4', 'Engineers', 'Group 4', 160.00, 158.50, -1.50, 10.00, 112.00, '11/2/2020', null, 128.58, 81.13, 4.00, 950, 'FALSE', 100, 0.0, 14, 'Engineers', '11/2/2020', 'Dummy Division 4', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('XY2', 'DUMMY NAME 5', 'Name 5', 'Last Name 5', 'Dummy Division 5', 'Engineers', 'Group 5', 160.00, 160.00, 0.00, 19.50, 136.00, '11/2/2020', null, 110.00, 68.75, 8.00, 1030, 'FALSE', 100, 0.0, 17, 'Engineers', '11/2/2020', 'Dummy Division 5', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('AB2', 'DUMMY NAME 6', 'Name 6', 'Last Name 6', 'Dummy Division 6', 'Engineers', 'Group 6', 160.00, 160.00, 0.00, 0.00, 144.00, '3/1/2021', null, 152.00, 95.00, 0.00, 1000, 'FALSE', 100, 0.0, 18, 'Engineers', '3/1/2021', 'Dummy Division 6', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('AB1', 'DUMMY NAME 7', 'Name 7', 'Last Name 7', 'Dummy Division 7', 'Engineers', 'Group 7', 160.00, 161.00, 1.00, 14.50, 88.00, '3/1/2021', null, 135.50, 84.16, 24.00, 1000, 'FALSE', 100, 0.0, 11, 'Engineers', '3/1/2021', 'Dummy Division 7', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('DE1', 'DUMMY NAME 9', 'Name 9', 'Last Name 9', 'Dummy Division 9', 'Engineers', 'Group 9', 160.00, 171.00, 11.00, 30.50, 104.00, '1/11/2021', null, 17.50, 10.23, 24.00, 1000, 'FALSE', 100, 0.0, 13, 'Engineers', '1/11/2021', 'Dummy Division 9', 'Engineers',  0, 0, 0, 0, 0);
+INSERT INTO EMPLOY_META_INFO(visa, name, FIRST_NAME, LAST_NAME, division, is_Mgr, GROUP_SA, working_Hours, worked_Hours, difference, cumul_Diff, vac_Left, entrance, resignation, ch_Prd, ch_Prd_Percent, absence, sort, forfait, part_Time, CALCULATED, calculated_Vac_Left, calculated_Is_Mgr, vn_Entry, calculated_Division, calculated_Overview, holidays, illness, trainings, onboarding, others) VALUES ('DE2', 'DUMMY NAME 10', 'Name 10', 'Last Name 10', 'Dummy Division 10', 'Engineers', 'Group 10', 160.00, 160.00, 0.00, 15.50, 140.00, '1/18/2021', null, 139.00, 86.88, 4.00, 1000, 'FALSE', 100, 0.0, 17.5, 'Engineers', '1/18/2021', 'Dummy Division 10', 'Engineers',  0, 0, 0, 0, 0);
+
+
+
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('ABC', '2022-1-4', '543123-101-44', 8, 'comment of task 1', 'dummy description', 'FALSE', '543123-101', 'dummy sub name', 543123, 'dummy project name', 0, 8, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('DEF', '2022-4-4', '543123-101-44', 8, 'comment of task 2', 'dummy description', 'FALSE', '543123-101', 'dummy sub name', 543123, 'dummy project name', 0, 8, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('AB2', '2022-5-4', '543123-101-44', 8, 'comment of task 3', 'dummy description', 'FALSE', '543123-101', 'dummy sub name', 543123, 'dummy project name', 0, 8, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('AB2', '2022-6-4', '19513-101-44', 8, 'Project X comment', 'dummy description', 'FALSE', '19513-101', 'dummy sub name', 19513, 'dummy project name', 0, 8, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('XYZ', '2022-7-4', '18020-101-33', 1, 'Project X comment', 'dummy description', 'FALSE', '18020-101', 'dummy sub name', 18020, 'dummy project name', 0, 1, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+INSERT INTO EMPLOYEE_MONTHLY_VERTEC (visa, DATE, code, hrs, comment, description, vn, SUBPROJECT, SUBPROJECT_NAME, PROJECT, project_Name, vn_Hrs, ch_Hrs, uniques, CALCULATED_SUBPROJECT_NAME, division) VALUES ('ABC', '2022-7-4', '19513-101-44', 7, 'Project Y', 'dummy description', 'FALSE', '19513-101', 'dummy sub name', 19513, 'dummy project name', 0, 7, 1, 'dummy project name, dummy sub name', 'reserve');
+
+
+
+
+
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+INSERT INTO EMPLOY_ROLE(active, EMPLOYEE_NR, last, first, abbreviation, contract, forfait, user_Level, level, sub_Level, part_Time, SUPERVISOR)
+VALUES (true, 23209, 'dummy last name', 'first name', 'ABC', 'B', 'N', 'Eng begin Vietnam', 'Level 1', .2, 100.00, 'XYZ');
+
+
+
+
+
+
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('ABC', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', '1', '1', '1', null, null, null, null, null, null, null, null, null);
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('DEF', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', '1', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('XYZ', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', null, null, '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('AB1', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('AB2', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
+INSERT INTO CAPACITY (visa, reserve_1, reserve_2, reserve_3, reserve_4, jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec) VALUES ('XY2', 'raw data 1', 'raw data 2', 'raw data 3', 'raw data 4', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
